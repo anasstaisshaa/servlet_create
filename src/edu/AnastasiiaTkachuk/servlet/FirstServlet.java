81a7b42efa37cb26730f929e804240758a3d5a91
Anastasiia Tkachuk <anastaisha.580@gmail.com>
2023-02-06 21:33:27 +0200

    Initial commit

---
+++ src/edu/AnastasiiaTkachuk/servlet/FirstServlet.java
@@ -0,0 +1,32 @@
+package edu.AnastasiiaTkachuk.servlet;
+
+import jakarta.servlet.ServletConfig;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+@WebServlet("/servlet")
+public class FirstServlet extends HttpServlet {
+    @Override
+    public void init(ServletConfig config) throws ServletException {
+        super.init(config);
+    }
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        resp.setContentType("text/html");
+        try (PrintWriter writer = resp.getWriter()) {
+            writer.write("Hello World from first servlet");
+        }
+    }
+
+    @Override
+    public void destroy() {
+        super.destroy();
+    }
+}
